c3650b3180266a2600ae44d6341c33b5d9d7b960
cteds_15 <cteds_15@LABPROG15>
2022-12-07 21:13:20 -0300

    Add project files.

---
+++ Data/Context.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace avaliacaod7.Data
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasData(GetUser());
+            base.OnModelCreating(modelBuilder);
+        }
+
+        private static User GetUser()
+        {
+            return new User
+            {
+                UserId = Guid.NewGuid(),
+                Email = "admin@email.com",
+                Password = "admin123"
+            };
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using avaliacaod7.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace avaliacaod7
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly Context context;
+        public MainWindow(Context context)
+        {
+            this.context = context;
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+
+            var users = context.Users.ToList();
+
+            foreach (var user in users)
+            {
+                if (user.Email == Email.Text)
+                {
+                    if (user.Password == Password.Text)
+                    {
+                        MessageBox.Show("Usuário logado com êxito!");
+                    }
+                    else MessageBox.Show("Senha Inválida");
+                }
+                else MessageBox.Show("Usuário não encontrado");
+            }
+
+        }
+    }
+}
